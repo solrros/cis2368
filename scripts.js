508aa3e010a1aca3cedb6d5300ba3c01e051a7b2
solrros <97253230+solrros@users.noreply.github.com>
2022-10-24 17:02:16 -0500

    Just working

---
+++ scripts.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+
+const btn = document.querySelector('#button');
+
+btn.addEventListener('click', () => {
+    console.log('button was clicked');
+    alert('You clicked the button');
+
+});
